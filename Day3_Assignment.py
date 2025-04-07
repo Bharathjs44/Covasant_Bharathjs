354143ef568d033e992505a6a82117dd7eeab41e
Bharatjs44 <bharathjakkamsetti99@gmail.com>
2025-04-07 20:24:54 +0530

    Day3 updated

--- Day3_Assignment.py
+++ Day3_Assignment.py
@@ -13,5 +13,28 @@ def flatten(mylist):
     
 list1= [1,2,3, [1,2,3,[3,4],2]]
 print("flattend list is: ",flatten(list1))
+
+#######################################################################################
+
+'''input = [[[ '(0,1,2)' , '(3,4,5)'], ['(5,6,7)' , '(9,4,2)']]]
+output = [[[[0,1,2],[3,4,5]],[[5,6,7],[9,4,2]]]]'''
+
+    
+#Q - converting into list
+
+data= [[[ '(0,1,2)' , '(3,4,5)'], ['(5,6,7)' , '(9,4,2)']]]
+
+converted =[]
+for outer in data:
+    outer_list =[]
+    for inner in outer:
+        inner_list=[]
+        for item in inner:
+            numbers=list(map(int,item.strip('()').split(',')))
+            inner_list.append(numbers)
+        outer_list.append(inner_list)
+    converted.append(outer_list)
+ 
+print("Converted list is:",converted) 
     
     
